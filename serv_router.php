36220be529d9a18268428f94523dc0544c807c04
flsouto <fabiolimasouto@gmail.com>
2024-10-17 00:00:40 -0300

    updates

--- serv_router.php
+++ serv_router.php
@@ -1,4 +1,11 @@
 <?php
 
 $args = file_get_contents(".serv_args_".$_SERVER['SERVER_PORT']);
-passthru("fcall ".$args);
+$argv = preg_split('/\s+(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)/', trim($args));
+
+$argv = array_map(function ($arg) {
+    return trim($arg, '"');
+}, $argv);
+
+require_once(__DIR__.'/fcall.php');
+fcall($argv[0], array_slice($argv,1));
