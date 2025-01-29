028d2caa753aba65b750fd12d4978e598834eb4a
flsouto <fabiolimasouto@gmail.com>
2025-01-28 22:25:16 -0300

    updates

--- jsondb.php
+++ jsondb.php
@@ -2,13 +2,30 @@
 
 class JsonDB extends ArrayObject{
 
-    public $data = [];
-
     function __construct(public $file){
         if(file_exists($file)){
             $contents = file_get_contents($file);
             parent::__construct(json_decode($contents,true));
         }
+    }
+
+    function keys(){
+        return array_keys($this->getArrayCopy());
+    }
+
+    function fstk(){
+        $keys = $this->keys();
+        return $keys[0] ?? null;
+    }
+
+    function lstk(){
+        $keys = $this->keys();
+        $key = end($keys);
+        return $key;
+    }
+
+    function gitpush($msg='updates'){
+        shell_exec("git add $this->file; git commit -m '$msg'; git push origin $(git branch --show-current)");
     }
 
     function __set($k,$v){
