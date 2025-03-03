0d82ce7aabb1e50968a98497669072a511cae4b1
flsouto <fabiolimasouto@gmail.com>
2025-03-03 12:41:26 -0300

    updates

--- mkvid.php
+++ mkvid.php
@@ -1,6 +1,10 @@
 <?php
 
 function mkvid($aud, $img, $out='output.mp4', $fps=10){
-    shell_exec("ffmpeg -y -t $(soxi -D '$aud') -r $fps -i $img -c:v libx264 -pix_fmt yuv420p -crf 23 -r $fps -y /tmp/frames.mp4");
-    shell_exec("ffmpeg -y -i /tmp/frames.mp4 -i $aud -c copy -map 0:v:0 -map 1:a:0 $out -shortest");
+    if(empty($aud)){
+        shell_exec("ffmpeg -y -r $fps -i $img -c:v libx264 -pix_fmt yuv420p -crf 23 -r $fps -y $out");
+    } else {
+        shell_exec("ffmpeg -y -t $(soxi -D '$aud') -r $fps -i $img -c:v libx264 -pix_fmt yuv420p -crf 23 -r $fps -y /tmp/frames.mp4");
+        shell_exec("ffmpeg -y -i /tmp/frames.mp4 -i $aud -c copy -map 0:v:0 -map 1:a:0 $out -shortest");
+    }
 }
--- ytiframe.php
+++ ytiframe.php
@@ -1,5 +1,18 @@
 <?php
 
-function ytiframe($vid_id){
-    return '<iframe width="560" height="315" src="https://www.youtube.com/embed/'.$vid_id.'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+function ytiframe(...$ids){
+    $vid_id = $ids[$_GET['index']??0];
+
+    $html = '<iframe width="560" height="315" src="https://www.youtube.com/embed/'.$vid_id.'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+
+    if(count($ids)>1){
+        $html .= '<hr/>';
+        foreach($ids as $i=>$id){
+            $html .= ($i == ($_GET['index']??0) ? ' &raquo; ' : '' ) . "<a href='?index=$i'>$id</a><br/>";
+        }
+    }
+
+    return $html;
+
+    return $html;
 }
