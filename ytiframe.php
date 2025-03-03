2e6e6dfc5f7adc9bb57f5e7d73ada194890d323f
flsouto <fabiolimasouto@gmail.com>
2025-03-03 14:49:31 -0300

    updates

--- ytiframe.php
+++ ytiframe.php
@@ -1,14 +1,20 @@
 <?php
+require_once('htag.php');
 
 function ytiframe(...$ids){
-    $vid_id = $ids[$_GET['index']??0];
+    $index = $_GET['index']??0;
+    $vid_id = $ids[$index];
 
     $html = '<iframe width="560" height="315" src="https://www.youtube.com/embed/'.$vid_id.'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
 
     if(count($ids)>1){
         $html .= '<hr/>';
         foreach($ids as $i=>$id){
-            $html .= ($i == ($_GET['index']??0) ? ' &raquo; ' : '' ) . "<a href='?index=$i'>$id</a><br/>";
+            $html .= ($i == $index ? ' &raquo; ' : '' ) . htag('a',[
+                'href' => '?index='.$i,
+                'hotkey' => $i === $index+1 ? 'n' : (($i === $index-1) ? 'p' : null),
+                $id
+            ]) . "<br/>";
         }
     }
 
